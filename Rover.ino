f16a5cb0ed4255085205ac8222a8f0ac767dab45
SoulPhoenix <67401648+SoulFirePhoenix@users.noreply.github.com>
2026-04-21 15:19:56 +1000

    Changed motor control and merged code

--- Rover.ino
+++ Rover.ino
@@ -1,66 +1,128 @@
-// Pin allocations
-// Pin  ( 2,3 are for Motor Left ) ( 4,5 are for Motor Right )
+//motor and sonar pin definition
+//motor1 being high and motor2 being low will cause the rover to move forward, vice versa for reverse
+#define left_motor1 11
+#define left_motor2 10
+#define right_motor1 7
+#define right_motor2 8
+#define sonar_trig 4
+#define sonar_echo 3
 
+//movement and turning time in milliseconds. will change when testing in maze 
+const int forward_time = 1000;
+const int turn90time = 400;
+const int turn180time = 800;
+//change above constants later
 
 void setup() {
-  // Motor Pin Def
-  // Left
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-
-  // Right
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  // put your setup code here, to run once:
+  pinMode(left_motor1, OUTPUT);
+  pinMode(left_motor2, OUTPUT);
+  pinMode(right_motor1, OUTPUT);
+  pinMode(right_motor2, OUTPUT);
+  pinMode(sonar_trig, OUTPUT);
+  pinMode(sonar_echo, INPUT);
 }
-
-
-//Function Defs
-
-// Steering Rover Control
-void Left_Turn() {
-  // Left
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-    
-  // Right
-  digitalWrite(4, LOW);
-  digitalWrite(5, HIGH);
-}
-
-void Right_Turn() {
-  // Left
-  digitalWrite(2, HIGH);
-  digitalWrite(3, LOW);
-  
-  // Right
-  digitalWrite(4, HIGH);
-  digitalWrite(5, LOW);
-}
-
-void Forward() {
-  // Left
-  digitalWrite(2, HIGH);
-  digitalWrite(3, LOW);
-
-  // Right
-  digitalWrite(4, LOW);
-  digitalWrite(5, HIGH);
-}
-
-void Backward() {
-    // Left
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-
-  // Right
-  digitalWrite(4, HIGH);
-  digitalWrite(5, LOW);
-}
-
-
 
 void loop() {
   // put your main code here, to run repeatedly:
+  forward();
+  rotate90left();
+  forward();
+  rotate90left();
+  forward();
+  rotate90left();
+  forward();
+  rotate180();
+  delay(500);
+  forward();
+  rotate90right();
+  forward();
+  rotate90right();
+  forward();
+  rotate90right();
+  forward();
+  rotate180();
+  delay(500);
 
+}
 
+//will turn the rover 90 degrees to its left on the spot
+void rotate90left() {
+  digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, LOW);
+  digitalWrite(right_motor2, LOW);
+  delay(200);
+
+  digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, HIGH);
+  digitalWrite(right_motor1, HIGH);
+  digitalWrite(right_motor2, LOW);
+  delay(turn90time);
+
+  digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, LOW);
+  digitalWrite(right_motor2, LOW);
+  delay(500);
+}
+
+void rotate90right() {
+  digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, LOW);
+  digitalWrite(right_motor2, LOW);
+  delay(200);
+
+  digitalWrite(left_motor1, HIGH);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, LOW);
+  digitalWrite(right_motor2, HIGH);
+  delay(turn90time);
+
+  digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, LOW);
+  digitalWrite(right_motor2, LOW);
+  delay(500);
+}
+
+void rotate180() {
+  digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, LOW);
+  digitalWrite(right_motor2, LOW);
+  delay(200);
+
+  digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, HIGH);
+  digitalWrite(right_motor1, HIGH);
+  digitalWrite(right_motor2, LOW);
+  delay(turn180time);
+
+  digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, LOW);
+  digitalWrite(right_motor2, LOW);
+  delay(500);
+}
+
+void forward() {
+   digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, LOW);
+  digitalWrite(right_motor2, LOW);
+  delay(200);
+
+  digitalWrite(left_motor1, HIGH);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, HIGH);
+  digitalWrite(right_motor2, LOW);
+  delay(forward_time);
+
+  digitalWrite(left_motor1, LOW);
+  digitalWrite(left_motor2, LOW);
+  digitalWrite(right_motor1, LOW);
+  digitalWrite(right_motor2, LOW);
+  delay(500);
 }
